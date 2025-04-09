6625f6e122920136f93134a7c3db5ae94be29a94
Kaiyuan Han <hermit_han@163.com>
2025-04-09 14:59:26 +0800

    Initial commit

---
+++ genomescope.info.lisp
@@ -0,0 +1,8 @@
+(:name           "genomescope")
+(:repo           "taffish-org/genomescope")
+(:author         "KaiyuanHan")
+(:version        "0.0.0")
+(:intro          "Fast genome analysis from unassembled short reads")
+(:dependencies)
+(:functions      "[R :: 4.4.3]"
+                 "genomescope")
